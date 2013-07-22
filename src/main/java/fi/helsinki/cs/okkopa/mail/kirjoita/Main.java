776f7fba9967d644b05a334d99dfe4f160b5252f
Antti J I Kaikkonen <anttkaik@dat-5.cs.helsinki.fi>
2013-07-22 12:07:24 +0300

    pakkauksia

---
+++ src/main/java/fi/helsinki/cs/okkopa/mail/kirjoita/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.helsinki.cs.okkopa.mail.kirjoita;
+import javax.mail.*;
+
+/**
+ *
+ * @author anttkaik
+ */
+public class Main {
+    
+}
