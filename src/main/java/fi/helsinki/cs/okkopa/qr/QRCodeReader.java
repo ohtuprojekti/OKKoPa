6dcfabc2c47ce67c0ecefc1bea70a2c8801ac471
Heikki Haapala <heikki.haapala@helsinki.fi>
2013-07-23 11:14:58 +0300

    testing

--- src/main/java/fi/helsinki/cs/okkopa/qr/QRCodeReader.java
+++ src/main/java/fi/helsinki/cs/okkopa/qr/QRCodeReader.java
@@ -8,13 +8,14 @@ import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 import java.awt.image.BufferedImage;
+import java.util.List;
 
 public class QRCodeReader {
 
-    Result readQRCode(BufferedImage image) throws NotFoundException {
-        LuminanceSource source = new BufferedImageLuminanceSource(image);
+    public void readQRCodes(List<ExamPaper> papers) throws NotFoundException {
+        LuminanceSource source = new BufferedImageLuminanceSource((BufferedImage) images.get(i));
         BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
         MultiFormatReader reader = new MultiFormatReader();
-        return reader.decode(bitmap);
+        Result result = reader.decode(bitmap);
     }
 }
