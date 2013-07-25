fd5bf7bc5b4be134edaa95d003f026b6780e7704
Heikki Haapala <heikki.haapala@helsinki.fi>
2013-07-25 10:20:50 +0300

    beginning of main

---
+++ src/main/java/fi/helsinki/cs/okkopa/OkkopaMain.java
@@ -0,0 +1,15 @@
+package fi.helsinki.cs.okkopa;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class OkkopaMain {
+
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduler =
+                Executors.newScheduledThreadPool(1);
+        OkkopaRunner runner = new OkkopaRunner();
+        scheduler.scheduleAtFixedRate(runner, 1, 1, TimeUnit.SECONDS);
+    }
+}
---
+++ src/main/java/fi/helsinki/cs/okkopa/OkkopaRunner.java
@@ -0,0 +1,11 @@
+package fi.helsinki.cs.okkopa;
+
+public class OkkopaRunner implements Runnable {
+    
+    
+
+    @Override
+    public void run() {
+        
+    }
+}
