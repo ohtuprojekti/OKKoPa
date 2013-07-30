25f21bfba98bcef821a1e316ad3dd119b3bab460
Amir2707 <Amir2707@gmail.com>
2013-07-30 13:50:39 +0300

    first story test

--- src/test/java/stories/readStory.groovy
+++ src/test/java/stories/readStory.groovy
@@ -6,17 +6,25 @@ import com.icegreen.greenmail.util.GreenMail
 import fi.helsinki.cs.okkopa.Settings
 import fi.helsinki.cs.okkopa.mail.read.IMAPserver
 import fi.helsinki.cs.okkopa.mail.read.MailRead
-ServerSetup setup
+import java.security.Security
+import com.icegreen.greenmail.util.DummySSLSocketFactory
+import com.icegreen.greenmail.user.GreenMailUser
 GreenMail greenMail
 Properties props
-
+MailRead mail;
 description "E-mail login"
  
 scenario "User can login with valid e-mail/password combination", {
- 
+    
+    
     given "",{
-       setup = new ServerSetup(4012, "localhost", ServerSetup.PROTOCOL_SMTPS);
-        greenMail = new GreenMail(setup); //uses test ports by default
+        ServerSetup setup = new ServerSetup(4012, "localhost", ServerSetup.PROTOCOL_SMTPS);
+        ServerSetup setup2 = new ServerSetup(4011, "localhost", ServerSetup.PROTOCOL_IMAPS);
+        ServerSetup[] setups = new ServerSetup[2];
+        setups[0] = setup;
+        setups[1] = setup2;
+        greenMail = new GreenMail(setups); //uses test ports by default
+        GreenMailUser user = greenMail.setUser("okkopa@localhost.com", "okkopa", "soooosecret");
         greenMail.start();
          props = (new Settings("src/test/resources/smtptestsettings.xml")).getSettings();
         Security.setProperty("ssl.SocketFactory.provider", DummySSLSocketFactory.class.getName());
@@ -26,34 +34,36 @@ scenario "User can login with valid e-mail/password combination", {
     }
  
     when "", {
-       MailRead mail = new MailRead(new Settings("src/test/resources/smtptestsettings.xml"))
+        mail = new MailRead(new Settings("/smtptestsettings.xml"))
         mail.connect()
     }
  
     then "user will be logged in to system", {
+        greenMail.stop()
     
     }
 }
-scenario "User can,t login with invalid e-mail/password combination", {
- 
-    given "",{
-        setup = new ServerSetup(4012, "localhost", ServerSetup.PROTOCOL_SMTPS);
-        greenMail = new GreenMail(setup); //uses test ports by default
-        greenMail.start();
-       props = (new Settings("src/test/resources/smtptestsettings.xml")).getSettings();
-        Security.setProperty("ssl.SocketFactory.provider", DummySSLSocketFactory.class.getName());
-        
-      
-        
-       
-    }
- 
-    when "", {
-        MailRead mail = new MailRead(new Settings("src/test/resources/smtptestsettings.xml"))
-        mail.connect()
-        
-    }
- 
-    then "user will not be logged in to system", {
-    }
-}
+//scenario "User can,t login with invalid e-mail/password combination", {
+// 
+//    given "",{
+//        setup = new ServerSetup(4012, "localhost", ServerSetup.PROTOCOL_SMTPS);
+//        greenMail = new GreenMail(setup); //uses test ports by default
+//        greenMail.start();
+//       props = (new Settings("/smtptestsettings.xml")).getSettings();
+//        Security.setProperty("ssl.SocketFactory.provider", DummySSLSocketFactory.class.getName());
+//        
+//      
+//        
+//       
+//    }
+// 
+//    when "", {
+//        MailRead mail = new MailRead(new Settings("src/test/resources/smtptestsettings.xml"))
+//        mail.connect()
+//        
+//    }
+// 
+//    then "user will not be logged in to system", {
+//        greenMail.stop()
+//    }
+//}
