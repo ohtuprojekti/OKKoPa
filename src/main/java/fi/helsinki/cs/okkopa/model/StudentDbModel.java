9733be837274249347ec43ac76005cb0835af20a
P Petteri Hemmila <phemmila@dat-6.cs.helsinki.fi>
2013-08-14 13:23:23 +0300

    Oracle DB stuff

---
+++ src/main/java/fi/helsinki/cs/okkopa/model/StudentDbModel.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.helsinki.cs.okkopa.model;
+
+/**
+ *
+ * @author phemmila
+ * */
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "OPISKELIJA")
+public class StudentDbModel {
+    
+    @DatabaseField(columnName = "OPNRO")    
+    private String studentNumber;
+    
+    public StudentDbModel() {
+        
+    }
+    
+    public StudentDbModel(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+}
