63f8e0fbb9077b9029da9c8aa976b094b0273e6b
P Petteri Hemmila <phemmila@dat-6.cs.helsinki.fi>
2013-08-21 11:27:50 +0300

    testing

--- src/main/java/fi/helsinki/cs/okkopa/mail/send/EmailSender.java
+++ src/main/java/fi/helsinki/cs/okkopa/mail/send/EmailSender.java
@@ -6,4 +6,6 @@ import javax.mail.MessagingException;
 public interface EmailSender {
 
     public void send(String receiverEmailAddress, InputStream attachment) throws MessagingException;
+    
+    public void send(String receiverEmailAddress, String subject, String message, String senderAddress, InputStream attachment) throws MessagingException;
 }
--- src/main/java/fi/helsinki/cs/okkopa/main/stage/ReadCourseInfoStage.java
+++ src/main/java/fi/helsinki/cs/okkopa/main/stage/ReadCourseInfoStage.java
@@ -3,16 +3,22 @@ package fi.helsinki.cs.okkopa.main.stage;
 import fi.helsinki.cs.okkopa.database.BatchDetailDAO;
 import fi.helsinki.cs.okkopa.database.OkkopaDatabaseConnectionSource;
 import fi.helsinki.cs.okkopa.exception.NotFoundException;
+import fi.helsinki.cs.okkopa.mail.send.EmailSender;
+import fi.helsinki.cs.okkopa.mail.send.EmailSenderImpl;
 import fi.helsinki.cs.okkopa.main.BatchDetails;
 import fi.helsinki.cs.okkopa.main.ExceptionLogger;
 import fi.helsinki.cs.okkopa.main.Settings;
 import fi.helsinki.cs.okkopa.model.BatchDbModel;
 import fi.helsinki.cs.okkopa.model.ExamPaper;
 import fi.helsinki.cs.okkopa.pdfprocessor.PDFProcessor;
+import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.List;
+import javax.mail.MessagingException;
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.jpedal.exception.PdfException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,13 +31,16 @@ public class ReadCourseInfoStage extends Stage<List<ExamPaper>, ExamPaper> {
     private ExceptionLogger exceptionLogger;
     private PDFProcessor pdfProcessor;
     private BatchDetails batch;
+    private EmailSender emailSender;
+    private Settings settings;
 
     @Autowired
-    public ReadCourseInfoStage(PDFProcessor pDFProcessor, ExceptionLogger exceptionLogger, BatchDetails batch) {
+    public ReadCourseInfoStage(PDFProcessor pDFProcessor, ExceptionLogger exceptionLogger, BatchDetails batch, Settings settings, EmailSender emailSender) {
         this.pdfProcessor = pDFProcessor;
         this.exceptionLogger = exceptionLogger;
         this.batch = batch;
-
+        this.settings = settings;
+        this.emailSender = emailSender;
     }
 
     @Override
@@ -39,7 +48,7 @@ public class ReadCourseInfoStage extends Stage<List<ExamPaper>, ExamPaper> {
         ExamPaper courseInfoPage = examPapers.get(0);
 
         batch.reset();
-        
+
 //        CourseInfo courseInfo = null;
         try {
             courseInfoPage.setPageImages(pdfProcessor.getPageImages(courseInfoPage));
@@ -50,7 +59,7 @@ public class ReadCourseInfoStage extends Stage<List<ExamPaper>, ExamPaper> {
             // a normal exam paper.
             examPapers.remove(0);
             LOGGER.debug("Kurssi-info luettu onnistuneesti.");
-        } catch (PdfException | NotFoundException  | SQLException | IOException ex) {
+        } catch (PdfException | NotFoundException | SQLException | IOException ex) {
             LOGGER.debug("jotain meni pieleen");
             exceptionLogger.logException(ex);
         }
@@ -61,6 +70,8 @@ public class ReadCourseInfoStage extends Stage<List<ExamPaper>, ExamPaper> {
             // Add course info (doesn't matter if null)
             processNextStages(examPaper);
         }
+
+        sendEmail();
     }
 
 //    public CourseInfo getCourseInfo(ExamPaper examPaper) throws NotFoundException {
@@ -75,22 +86,36 @@ public class ReadCourseInfoStage extends Stage<List<ExamPaper>, ExamPaper> {
     public void setBatchDetails(ExamPaper examPaper) throws SQLException, FileNotFoundException, IOException, NotFoundException {
         String[] fields = (examPaper.getQRCodeString() + ":236").split(":");
         LOGGER.debug("Kurssi-info luettu: " + examPaper.getQRCodeString());
-        batch.setCourseCode(fields[0]);
-        batch.setPeriod(fields[1]);
-        batch.setYear(Integer.parseInt(fields[2]));
-        batch.setType(fields[3]);
-        batch.setCourseNumber(Integer.parseInt(fields[4]));
 
+        try {
+            batch.setCourseCode(fields[0]);
+            batch.setPeriod(fields[1]);
+            batch.setYear(Integer.parseInt(fields[2]));
+            batch.setType(fields[3]);
+            batch.setCourseNumber(Integer.parseInt(fields[4]));
+        } catch (Exception e) {
+            throw new NotFoundException();
+        }
         if (fields.length >= 6) {
             BatchDetailDAO batchDao = new BatchDetailDAO(new OkkopaDatabaseConnectionSource(new Settings("settings.xml")));
             batchDao.addBatchDetails(new BatchDbModel("236", "Viesti kannasta", "okkopa.2013@gmail.com"));
             BatchDbModel bdm;
-            
+
             bdm = batchDao.getBatchDetails(fields[5]);
 
             batch.setEmailContent(bdm.getEmailContent());
             batch.setReportEmailAddress(bdm.getReportEmailAddress());
         }
 
+    }
+
+    private void sendEmail() {
+        try {
+            LOGGER.debug("Lähetetään sähköposti.");
+            emailSender.send(batch.getReportEmailAddress(), "subject settareista", "katenoi sisältö kasaan " + batch.getTotalPages() + "/" + batch.getFailedScans(), "okkopa.", null);
+        } catch (MessagingException ex) {
+            LOGGER.debug("Raporttisähköpostin lähetys epäonnistui.");
+            exceptionLogger.logException(ex);
+        }
     }
 }
